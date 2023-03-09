56a989115ea7523eb83aad2d584718af849fc1c9
alexeytkachenko1 <alexeytkachenko1@gmail.com>
2023-03-09 11:20:13 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const message = "Node.js in amazing!";
+
+// const Joi = require("joi");
+const shortid = require("shortid");
+
+// console.log(message);
+
+// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+// console.log(passwordSchema.validate("dsdsdsds$"));
+console.log(shortid.generate());
